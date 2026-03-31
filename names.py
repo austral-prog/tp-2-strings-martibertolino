870381d28bd22c9d560e2221068ffb5a191df8cf
martibertolino <virginiacesarini195@gmail.com>
2026-03-31 12:35:06 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,10 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+    one=input("ingrese su nombre")
+    two=input("ingrese su apellido")
+    name=f"{one} {two}"
+    print(name.lower())
+    print(name.title())
+    print(name.upper())
+    print("\t"+name.lower())
